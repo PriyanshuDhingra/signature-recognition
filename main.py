ce77cdcf5b0b5bb65177d4f0c91b76fcc728edd6
PriyanshuDhingra <priyanshudhingra1@gmail.com>
2024-07-26 13:25:45 +0530

    jupyter-notebook-done

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Comments:
+# Folder Structure:
+# src is the main file which contains all pipelines which we will build
+# Whatever we write in src pipeline, we can use/importing in other pipelines.
+# Env is signatureRecognition
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = "src", #Source
+    version = "0.0.1",
+    author = "Priyanshu_Dhingra",
+    author_email = "priyanshudhingra1@gmail.com",
+    packages = find_packages(), #Whatever written inside requirements.txt will go inside this
+    install_requires = [] # For installing packages mentioned inside requirements.txr
+)
